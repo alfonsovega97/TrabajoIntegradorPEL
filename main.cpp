366a7a83ed33a73f714f2bd21fad2a7434891d2a
alfonsovega97 <alfonsovega1005@hotmail.com>
2019-11-27 22:49:28 +0100

    V2 ahora se puede salir del menu

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 void menuRectas(){
     int op;
-    cout<<"---- MENU RECTAS ----\n";
+    bool ret = false;
+    cout<<"\n---- MENU RECTAS ----\n";
     cout<<"[ 1 ] En el plano\n";
     cout<<"[ 2 ] En el espacio\n";
+    cout<<"[ 0 ] Volver atras\n";
     cout<<"Que desea hacer?: ";
     cin>>op;
     do {
@@ -14,15 +16,21 @@ void menuRectas(){
                 break;
             case 2:
                 break;
+            case 0:
+                cout<<"\n";
+                ret = true;
+                break;
         }
-    }while(op>0 && op<3);
+    }while(op>0 && op<3 && !ret);
 }
 
 void menuPlanos(){
     int op;
-    cout<<"---- MENU PLANOS ----\n";
-    cout<<"[ 1 ] En el plano\n";
-    cout<<"[ 2 ] En el espacio\n";
+    bool ret = false;
+    cout<<"\n---- MENU PLANOS ----\n";
+    cout<<"[ 1 ] Dos planos\n";
+    cout<<"[ 2 ] Tres planos\n";
+    cout<<"[ 0 ] Volver atras\n";
     cout<<"Que desea hacer?: ";
     cin>>op;
     do {
@@ -31,8 +39,12 @@ void menuPlanos(){
                 break;
             case 2:
                 break;
+            case 0:
+                cout<<"\n";
+                ret = true;
+                break;
         }
-    }while(op>0 && op<3);
+    }while(op>0 && op<3 && !ret);
 }
 
 int main() {
@@ -41,13 +53,14 @@ int main() {
     cout<<"Autores: Carlos Valle, Julian de Pablo, Alfonso Vega, David Merle\n";
     cout<<"-----------------------------------------------------------------\n\n";
     int op;
-    cout<<"---- POSICIONES A ESTUDIAR ----\n";
-    cout<<"[ 1 ] Recta - recta\n";
-    cout<<"[ 2 ] Recta - plano\n";
-    cout<<"[ 3 ] Plano - plano\n";
-    cout<<"Que desea hacer?: ";
-    cin>>op;
+    bool exit = false;
     do {
+        cout<<"---- POSICIONES A ESTUDIAR ----\n";
+        cout<<"[ 1 ] Recta - recta\n";
+        cout<<"[ 2 ] Recta - plano\n";
+        cout<<"[ 3 ] Plano - plano\n";
+        cout<<"Que desea hacer?: ";
+        cin>>op;
         switch(op){
             case 1:
                 menuRectas();
@@ -57,6 +70,10 @@ int main() {
             case 3:
                 menuPlanos();
                 break;
+            case 0:
+                cout<<"Nos vemos pronto!";
+                exit = true;
+                break;
         }
-    }while(op>0 && op<4);
+    }while(op>0 && op<4 && !exit);
 }
